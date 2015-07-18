99d5ac407501b4d25987ca749159fa8ce912b4d7
actck <actck@hotmail.com>
2015-07-19 03:53:37 +0800

    js

---
+++ library.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 7/19/2015.
+ */
+(function(CDN){
+	"use strict";
+
+	CDN.onLoad = function(params ,callback){
+
+		console.log(123);
+		callback();
+	}
+
+
+
+})(module.exports);
